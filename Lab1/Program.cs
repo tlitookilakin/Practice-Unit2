52bf7df922bc8bdf6dadd031f7061fb9671cd201
Wren <grahamgrasley@gmail.com>
2024-08-05 14:53:39 -0400

    Lab 1
    Fixed indentation

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -4,14 +4,14 @@
 	{
 		static void Main(string[] args)
 		{
-            Console.WriteLine("Welcome to the room calculator!");
+			Console.WriteLine("Welcome to the room calculator!");
 
 			// get information
 
-            Console.WriteLine("Please enter the width of the room:");
+			Console.WriteLine("Please enter the width of the room:");
 			var width = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("Please enter the depth of the room:");
+			Console.WriteLine("Please enter the depth of the room:");
 			var depth = double.Parse(Console.ReadLine());
 
 			Console.WriteLine("Please enter the height of the room:");
@@ -40,13 +40,13 @@
 
 			Console.WriteLine($"\nThis is a {size} room,");
 			Console.WriteLine($"with a floor area of {area} and perimeter of {perimeter},");
-            Console.WriteLine($"as well as a volume of {volume} and surface area of {surface}");
+			Console.WriteLine($"as well as a volume of {volume} and surface area of {surface}");
 
 			// goodbye
 
 			Console.WriteLine("\nThank you for using the Room Calculator! Have a nice day!");
-            Console.WriteLine("Press any key to exit...");
+			Console.WriteLine("Press any key to exit...");
 			Console.ReadKey();
-        }
+		}
 	}
 }
