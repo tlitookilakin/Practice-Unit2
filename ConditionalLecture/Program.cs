12637ec736136935b74e515202b2541d9fc4f7ea
Wren <grahamgrasley@gmail.com>
2024-08-07 13:28:43 -0400

    ConditionalLecture
    Initial Commit

---
+++ ConditionalLecture/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace ConditionalLecture;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool isHungry = true;
+
+        if (isHungry)
+            Console.WriteLine("You are hungry!");
+        else 
+            Console.WriteLine("You are not hungry");
+
+        int temp = 123;
+
+        if (temp >= 212)
+            Console.WriteLine("Boiling!");
+        else if (temp <= 32)
+            Console.WriteLine("Freezing!");
+        else
+            Console.WriteLine("Normal.");
+
+        for (int i = 10; i >= 0; i--)
+            Console.WriteLine(i);
+
+        string[] items = ["tom", "david", "john"];
+        foreach (string item in items)
+            Console.WriteLine(item);
+
+        bool run = true;
+        while (run)
+        {
+            while (true)
+            {
+                var entry = Console.ReadLine();
+                if (entry is not null && entry.Length != 0) 
+                {
+                    if (entry[0] is 'Y' or 'y')
+                    {
+                        break;
+                    }
+                    else if (entry[0] is 'N' or 'n')
+                    {
+                        run = false;
+                        break;
+                    }
+                }
+
+                Console.WriteLine("Invalid answer, please enter [y] or [n]");
+            }
+        }
+    }
+}
---
+++ MethodLecture/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace MethodLecture
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			MyMethod();
+			DisplayGreeting("Wren");
+			Console.WriteLine(Adder(2, 3));
+			Console.WriteLine(AreaOfTriangle(13.0, 8.0));
+            Console.WriteLine(GetSalary(Console.ReadLine()));
+
+
+            Console.ReadKey();
+		}
+
+		static void MyMethod()
+		{
+            Console.WriteLine("I just ran!");
+        }
+
+		static void DisplayGreeting(string name)
+		{
+            Console.WriteLine($"Hello {name}!");
+        }
+
+		static int Adder(int x, int y)
+		{
+			return x + y;
+		}
+
+		static double AreaOfTriangle(double height, double length)
+		{
+			return (height * length) / 2.0;
+		}
+
+		static double GetSalary(string job)
+		{
+			switch (job)
+			{
+				case "Janitor":
+					return 50_000.0;
+				case "Developer":
+					return 70_000.0;
+				case "CEO":
+					return 100_000.0;
+				default:
+					return 0.0;
+			}
+		}
+	}
+}
